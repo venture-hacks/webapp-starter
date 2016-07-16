0726b1535345c8d5a7f730d6907fe654fb971590
austin <austin.cawley@gmail.com>
2016-07-16 03:32:59 -0400

    Step 1: Express

--- server.js
+++ server.js
@@ -6,10 +6,3 @@
 // __dirname is a node variable that holds the current working directory path
 var config = require(__dirname + '/config');
 var API_BASE = '/' + config.apiBase;
-var express = require('express');
-var app = express();
-
-// The first parameter is the port, the second is an optional callback on success.
-app.listen(8080, function() {
-    console.log("App is running on localhost:8080");
-});
